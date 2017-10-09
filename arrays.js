3c19f77a745ea4fb42602cec70938c98a294f32d
Andrea Piccioni <andpiccioni@gmail.com>
2017-10-09 09:48:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -17,7 +17,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  array.slice(0)
+  array.slice()
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
