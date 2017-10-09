61c6a60dc770a397ceb9c5a13c51f02f02f02f75
Andrea Piccioni <andpiccioni@gmail.com>
2017-10-09 10:20:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat","skittles"]
 
-function addElementToBeginningOfArray(array, element){
-  [element, ...chocolateBars]
-}
+
 
 function destructivelyAddElementToEndOfArray(array, element){
   array.push(element)
