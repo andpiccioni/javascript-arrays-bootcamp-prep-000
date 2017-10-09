856938d20fcf10591ed70181196c0733955345fb
Andrea Piccioni <andpiccioni@gmail.com>
2017-10-09 11:00:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,6 +31,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  array.slice()
-  return array
+  arraymod = array - array.slice(0)
+  return arraymod
 }
